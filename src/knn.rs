f356a7c6d55ee191b253d1c11220ec98cf32066f
Will Bradley <williambbradley@gmail.com>
2023-05-28 12:01:34 -0600

    updates

---
+++ src/knn.rs
@@ -0,0 +1,102 @@
+//! # KNN
+//!
+//! `knn` provides for fast method of finding exact k nearest neighbours
+//! for higher dimensional data. Also supports custom distance function.
+
+use crate::heap::Heap;
+
+#[derive(Clone)]
+/// The core datastructure of the package.
+/// Stores the points and the distance function.
+pub struct PointCloud<'a, T> {
+    // stores all points
+    pub points: Vec<&'a T>,
+
+    // return the distance given 2 ponints
+    dist_fn: fn(&T, &T) -> f64,
+}
+
+impl<'a, T> PointCloud<'a, T> {
+    /// Constructs a new `PointCloud<T>`.
+    ///
+    /// It accepts a function to compute distance between any 2 objects.
+    ///
+    /// # Examples
+    /// ```
+    /// extern crate knn;
+    /// use knn::PointCloud;
+    /// let manhattan_dist = |p: &[f64;2], q: &[f64;2]| -> f64 {(p[0] - q[0]).abs() + (p[1] - q[1]).abs()};
+    /// let pc = PointCloud::new(manhattan_dist);
+    /// ```
+    pub fn new(dist_fn: fn(&T, &T) -> f64) -> Self {
+        Self {
+            points: Vec::new(),
+            dist_fn: dist_fn,
+        }
+    }
+
+    /// Adds a point to the PointCloud.
+    ///
+    /// It accepts a reference to an object.
+    ///
+    /// # Examples
+    /// ```
+    /// extern crate knn;
+    /// use knn::PointCloud;
+    /// let dummy_dist = |p: &[f64;2], q: &[f64;2]| -> f64 {0.0};
+    /// let mut pc = PointCloud::new(dummy_dist);
+    /// let p = [1.89, 5.63];
+    /// pc.add_point(&p);
+    /// ```
+    pub fn add_point(&mut self, p: &'a T) {
+        self.points.push(p)
+    }
+
+    /// Gets the k nearest neighbours of an object.
+    ///
+    /// It accepts a reference to the object and how many neighbours to return.
+    ///
+    /// # Example
+    /// ```
+    /// extern crate knn;
+    /// use knn::PointCloud;
+    /// let manhattan_dist = |p: &[f64;2], q: &[f64;2]| -> f64 {(p[0] - q[0]).abs() + (p[1] - q[1]).abs()};
+    /// let mut pc = PointCloud::new(manhattan_dist);
+    /// let points = vec![[1.0, 1.0], [2.0, 2.0], [3.0, 2.0]];
+    /// for p in &points {
+    ///     pc.add_point(&p);
+    /// }
+    /// let q = [0.5, 0.5];
+    /// pc.get_nearest_k(&q, 2);
+    ///
+    /// ```
+    pub fn get_nearest_k(&self, p: &T, k: usize) -> Vec<(f64, &T)> {
+        let mut h = Heap::new(k);
+        for i in 0..self.points.len() {
+            let d = (self.dist_fn)(self.points[i], p);
+            h.insert(d, &self.points[i]);
+        }
+        let mut knn = Vec::new();
+        for i in h.get_elements() {
+            knn.push((i.0, *i.1));
+        }
+        knn
+    }
+
+    /// Get the len / number of objects in PointCloud
+    /// Example:
+    /// ```
+    /// extern crate knn;
+    /// use knn::PointCloud;
+    /// let manhattan_dist = |p: &[f64;2], q: &[f64;2]| -> f64 {(p[0] - q[0]).abs() + (p[1] - q[1]).abs()};
+    /// let mut pc = PointCloud::new(manhattan_dist);
+    /// let points = vec![[1.0, 1.0], [2.0, 2.0], [3.0, 2.0]];
+    /// for p in &points {
+    ///     pc.add_point(&p);
+    /// }
+    /// pc.len();
+    /// ```
+    pub fn len(&self) -> usize {
+        self.points.len()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,128 @@
+use crate::knn::PointCloud;
+use rand::Rng;
+use std::collections::HashSet;
+use std::f64::consts::TAU;
+use std::time::{Duration, Instant};
 use svg::node::element::path::Data;
-use svg::node::element::Path;
+use svg::node::element::{Circle, Path};
 use svg::Document;
+use vector2d::Vector2D;
 
+mod heap;
+mod knn;
+
+type V2 = Vector2D<f64>;
 type Result<T> = std::result::Result<T, Error>;
+const MAZE_RADIUS: f64 = 500.0;
+const MIN_SPACING: f64 = 2.0 / 50.0 * MAZE_RADIUS;
+const TUBE_RADIUS: f64 = 0.01 * MAZE_RADIUS;
+const COMPUTE_TIME: Duration = Duration::from_secs(3);
 
 #[derive(Debug)]
 pub struct Error(String);
 
+fn dist(a: &V2, b: &V2) -> f64 {
+    (a - b).length_squared()
+}
 impl From<std::io::Error> for Error {
     fn from(e: std::io::Error) -> Self {
         Self(e.to_string())
     }
 }
-fn main() -> Result<()> {
-    let data = Data::new()
-        .move_to((10, 10))
-        .line_by((0, 50))
-        .line_by((50, 0))
-        .line_by((0, -50))
-        .close();
-    let path = Path::new()
-        .set("fill", "none")
-        .set("stroke", "black")
-        .set("stroke-width", 3)
-        .set("d", data);
 
-    let document = Document::new().set("viewbox", (0, 0, 70, 70)).add(path);
+fn main() -> Result<()> {
+    let mut rng = rand::thread_rng();
+    let mut points: Vec<V2> = Vec::new();
+    let start = Instant::now();
+    let mut tries = 0;
+    while Instant::now() - start < COMPUTE_TIME {
+        let radians: f64 = rng.gen::<f64>() * TAU;
+        let radius: f64 = rng.gen::<f64>() * (MAZE_RADIUS - TUBE_RADIUS * (2f64).sqrt() * 2.0);
+        let point = V2 {
+            x: radians.cos() * radius,
+            y: radians.sin() * radius,
+        };
+        tries += 1;
+        if points
+            .iter()
+            .cloned()
+            .all(|a| (a - point).length() > MIN_SPACING)
+        {
+            points.push(point);
+        }
+    }
+    eprintln!("scanned {} points.", tries);
+    let mut document = Document::new().set(
+        "viewBox",
+        (
+            -MAZE_RADIUS,
+            -MAZE_RADIUS,
+            2.0 * MAZE_RADIUS,
+            2.0 * MAZE_RADIUS,
+        ),
+    );
+    let start: V2 = Pol {
+        a: -TAU / 3.0,
+        r: MAZE_RADIUS + TUBE_RADIUS * 10.0,
+    }
+    .into();
+    let end: V2 = Pol {
+        a: -2.0 * TAU / 3.0,
+        r: MAZE_RADIUS + TUBE_RADIUS * 10.0,
+    }
+    .into();
+
+    let edges: HashSet<(usize, usize)> = Default::default();
+    let mut cloud = PointCloud::new(dist);
+    points.iter().for_each(|p| cloud.add_point(p));
+    let Some(&(_, &start_point)) = cloud.get_nearest_k(&start, 1).first() else {
+        panic!("no start point!?");
+    };
+
+    document = document.add(
+        Circle::new()
+            .set("r", MAZE_RADIUS)
+            .set("cx", 0.0)
+            .set("cy", 0.0)
+            .set("fill", "black"),
+    );
+    for V2 { x, y } in points {
+        document = document.add(
+            Circle::new()
+                .set("r", TUBE_RADIUS)
+                .set("cx", x)
+                .set("cy", y)
+                .set("fill", "white"),
+        );
+    }
+    document = add_edge(document, start, start_point);
     svg::save("image.svg", &document)?;
     Ok(())
+}
+
+fn add_edge(document: Document, start: V2, end: V2) -> Document {
+    eprintln!("[add_edge] start={start:?} end={end:?}");
+    let data = Data::new()
+        .move_to((start.x, start.y))
+        .line_to((end.x, end.y));
+    let path = Path::new()
+        .set("fill", "red")
+        .set("stroke", "#ff000077")
+        .set("stroke-width", TUBE_RADIUS * 2.0)
+        .set("d", data);
+    document.add(path)
+}
+#[derive(Debug, Clone, Copy)]
+struct Pol {
+    pub a: f64,
+    pub r: f64,
+}
+
+impl From<Pol> for V2 {
+    fn from(p: Pol) -> Self {
+        Self {
+            x: p.a.cos() * p.r,
+            y: p.a.sin() * p.r,
+        }
+    }
 }
